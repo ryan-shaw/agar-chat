48c86b61cc614853f6837e974ba2deb32c53d4d6
Ryan Shaw <ryan.shaw@min.vc>
2015-05-29 18:04:44 +0100

    Rate limit

--- server/app.js
+++ server/app.js
@@ -10,9 +10,16 @@ io.on('connection', function(socket){
         socket.join(data.room);
         socket.nick = data.nick;
         console.log('Joining room: ' + data.room + ', username: ' + data.nick);
+        socket.last_message = Date.now();
     });
 
     socket.on('message', function(msg){
+        if((Date.now() - (1000 * 5)) < socket.last_message){
+            socket.emit('message', {nick: 'System', msg: 'PLEASE ONLY SEND EVERY 5 SECONDS'});
+            soket.last_message = Date.now();
+            return;
+        }
+        socket.last_message = Date.now();
         msg = msg.trim();
         if(msg.length === 0) return;
         io.emit('message', {nick: socket.nick, msg: msg});
